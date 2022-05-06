2ae4f7164df7371657490d7796ff7aa36b40141e
Paul Derscheid <paul.derscheid@lmscloud.de>
2022-05-06 12:39:18 +0000

    Update setup.sh

---
+++ setup.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+mkdir -p ~/git && cd ~/git
+
+echo '##### Fetching koha-testing-docker... #####'
+git clone https://gitlab.com/LMSCloudPaulD/koha-testing-docker-lmscloud-devel.git && echo '##### Fetching complete! #####'
+
+echo '##### Fetching kohaclone... #####'
+git clone https://gitlab.com/koha-community/Koha.git && echo '##### Fetching complete! Renaming ... #####'
+
+if [[ ${pwd} == /home/${USER}/git && -d Koha ]]; then
+    mv Koha kohaclone
+else
+    echo '##### Wrong working directory! Switching.. #####'
+    if [[ -n ${USER} ]]; then
+        cd /home/${USER}/git
+        mv Koha kohaclone
+    else
+        echo "##### ${USER} undefined! Exiting.. #####" && exit 1
+    fi
+fi
+echo '##### Done! #####'
+
+echo '##### Appending environment variables to bashrc.. #####'
+KOHA_TESTING_DOCKER_HOME="/home/${USER}/git/koha-testing-docker-lmscloud-devel"
+cat << EOF >> /home/${USER}/.bashrc
+# ENV variables for kohadevbox
+export SYNC_REPO="/home/${USER}/git/kohaclone"
+export LOCAL_USER_ID=$(id -u)
+export KOHA_TESTING_DOCKER_HOME=${KOHA_TESTING_DOCKER_HOME}
+source ${KOHA_TESTING_DOCKER_HOME}/files/bash_aliases
+EOF
+echo '##### Done! #####'
+
+echo '##### Switching to koha-testing-docker.. #####'
+cd /home/${USER}/git/koha-testing-docker-lmscloud-devel && echo '##### Copying defaults.. #####'
+cp env/defaults.env .env && echo 'Done!'
+
+echo '##### Maybe test the setup with "ku" #####'
+
+
