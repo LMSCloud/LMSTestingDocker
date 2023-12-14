9d81f83f9aa77b3a86bad0b36b496151af79b864
Paul Derscheid <paul.derscheid@lmscloud.de>
2023-12-14 19:18:49 +0100

    feat: migrate to [lms-image-builder](https://github.com/LMSCloudPaulD/lms-image-builder)
    - Remove arm dependencies now avaiable at community repositories.
    - Remove Dockerfile backup.
    - Remove old build scripts.
    - Remove deb package as it is no longer static.
    - Add .gitignore.

--- dists/lmscloud/get_lmscloud_release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget -q --output-document=current-koha-package.conf https://orgaknecht.lmscloud.net/lms-cloud-packages/packages/current-koha-package.conf || exit 1
-KOHAPACKAGE=$(head -n1 current-koha-package.conf | awk '{print $1;}') || exit 1
-rm -f current-koha-package.conf || exit 1
-wget -q --output-document="$KOHAPACKAGE" https://orgaknecht.lmscloud.net/lms-cloud-packages/packages/"$KOHAPACKAGE" || exit 1
